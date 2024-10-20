5c778ae60b2adab18b902e6130651bae5244ad0c
helana emad <helanaemad321@gmail>
2024-10-20 12:52:33 +0300

    add login_page.dart add login_view_body.dart

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,17 +1,24 @@
 import 'package:ecomerce_ieee/features/example_on_clean_dontwritethingonit/presentation/pages/home_screen.dart';
+import 'package:ecomerce_ieee/features/login/presentation/widgets/login_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoute {
   static const String home = "/";
+  static const String login = "/";
 
 
   static final router = GoRouter(
+
     routes: [
       GoRoute(
-        path: home,
-        builder: (context, state) => const HomeScreen(),
-      )
+        path: login,
+        builder: (context, state) => const LoginPage(), // هنا يتم تعريف صفحة تسجيل الدخول
+      ),
+
     ],
   );
+
+
+
 }
