f97ed38b6c350d38b568e68b35b807146ccf57fb
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-10-25 23:37:11 +0300

    refactor header

--- lib/features/brand/presentation/views/brand_view.dart
+++ lib/features/brand/presentation/views/brand_view.dart
@@ -1,8 +1,7 @@
-import 'package:ecomerce_ieee/core/utils/app_color.dart';
 import 'package:ecomerce_ieee/features/brand/presentation/widgets/custom_brand_list.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../core/utils/app_style.dart';
+import '../widgets/text_header.dart';
 
 class BrandView extends StatelessWidget {
   const BrandView({super.key});
@@ -15,25 +14,7 @@ class BrandView extends StatelessWidget {
           padding: const EdgeInsets.only(top: 40, left: 20),
           child: CustomScrollView(
             slivers: [
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Categories",
-                        style: AppStyle.style18
-                            .copyWith(color: AppColor.brandTextColor),
-                      ),
-                      Text(
-                        "See All",
-                        style: AppStyle.style12,
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              const SliverToBoxAdapter(child: TextHeader()),
               SliverToBoxAdapter(
                 child: SizedBox(
                   height: 16,
